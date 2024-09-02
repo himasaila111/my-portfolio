364ffc24100bd10144e2d8a79721b25a0f134e02
himasaila111 <himasaila24@gmail.com>
2024-09-01 20:22:42 -0400

    base path added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,16 +27,16 @@ export const Navbar = () => {
           onClick={() => setMenuOpen(false)}
         >
           <li>
-            <a href="#about">About</a>
+            <a href="himasaila/#about">About</a>
           </li>
           <li>
-            <a href="#experience">Experience</a>
+            <a href="himasaila/#experience">Experience</a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="himasaila/#projects">Projects</a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="himasaila/#contact">Contact</a>
           </li>
         </ul>
       </div>
